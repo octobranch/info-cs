5b17b3b7bb266b1ad77d137f2875a1b74af32eba
Octo-Branch <qkike@icloud.com>
2025-03-21 21:50:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-Configuración del widget de tiempo
-time_is_widget.init({
-    time_San_Jose_del_Cabo: {
-        id: "San_José_del_Cabo_z155",
-        template: "TIME<br>DATE",
-        time_format: "hours:minutes",
-        date_format: "dayname daynum/monthnum/yy",
-        coords: "23.0540600,-109.7036700"
-    },
-    time_Puerto_Vallarta: {
-        id: "Puerto_Vallarta_z159",
-        template: "TIME<br>DATE",
-        time_format: "hours:minutes",
-        date_format: "dayname daynum/monthnum/yy",
-        coords: "20.6170000,-105.2301800"
-    },
-    time_Punta_Cana: {
-        id: "Punta_Cana_z17a",
-        template: "TIME<br>DATE",
-        time_format: "hours:minutes",
-        date_format: "dayname daynum/monthnum/yy",
-        coords: "18.5818200,-68.4043100"
-    },
-    time_Cancun: {
-        id: "Cancún_z11f",
-        template: "TIME<br>DATE",
-        time_format: "hours:minutes",
-        date_format: "dayname daynum/monthnum/yy",
-        coords: "21.1742900,-86.8465600"
-    }
-});
-
-// Efecto de partículas
+// Efecto de partículas dinámicas
 function createParticles() {
     const particles = document.querySelector('.quantum-particles');
     for(let i = 0; i < 200; i++) {
@@ -43,7 +11,7 @@ function createParticles() {
 }
 createParticles();
 
-// Cursor interactivo
+// Efecto de cursor interactivo
 document.addEventListener('mousemove', (e) => {
     const cursor = document.querySelector('.interactive-cursor');
     cursor.style.left = e.clientX + 'px';
@@ -58,7 +26,7 @@ document.addEventListener('mousemove', (e) => {
     });
 });
 
-// Efectos de sonido
+// Efectos de sonido mejorados
 const hoverSound = new Audio('https://assets.mixkit.co/active_preview/sfx/2681/2681-preview.mp3');
 const clickSound = new Audio('https://assets.mixkit.co/active_preview/sfx/2697/2697-preview.mp3');
 
@@ -66,13 +34,23 @@ document.querySelectorAll('.time-box').forEach(box => {
     box.addEventListener('mouseenter', () => {
         hoverSound.currentTime = 0;
         hoverSound.play();
+        anime({
+            targets: box,
+            scale: 1.05,
+            duration: 500,
+            easing: 'easeOutExpo'
+        });
     });
 
     box.addEventListener('click', () => {
         clickSound.play();
-        box.style.transform = `rotateX(15deg) rotateY(-15deg) translateZ(70px)`;
-        setTimeout(() => {
-            box.style.transform = `rotateX(10deg) rotateY(-10deg) translateZ(50px)`;
-        }, 300);
+        anime({
+            targets: box,
+            scale: [1.1, 1],
+            rotateX: [15, 0],
+            rotateY: [-15, 0],
+            duration: 800,
+            easing: 'easeOutElastic(1, .5)'
+        });
     });
 });
